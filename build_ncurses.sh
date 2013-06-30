aa7b833b516e09d3415a24da5f8521514d9aa390
Skrilax_CZ <skrilax@gmail.com>
2013-06-30 21:30:17 +0200

    Modify ncurses configuration

--- build_ncurses.sh
+++ build_ncurses.sh
@@ -6,6 +6,7 @@ export ROOT_DIR=/home/skrilax/arm/build
 export SRC_DIR=/home/skrilax/arm/src
 
 export PACKAGE=ncurses-5.9
+export PACKAGE_ARGS="--with-shared --without-debug --without-ada --enable-overwrite --with-build-cc=gcc"
 export LOGFILE=ncurses.log
 
 ## Package
