030562079dadcd2a8eb2447988d14ae4b9c73776
Skrilax_CZ <skrilax@gmail.com>
2013-07-01 15:26:48 +0200

    Add systemd

---
+++ build_systemd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## VARIABLES
+
+export SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export ROOT_DIR=$SRC_DIR/../build
+
+export PACKAGE=systemd-204
+export PACKAGE_ARGS="--disable-nls"
+export LOGFILE=systemd.log
+
+## Package
+source $SRC_DIR/package_check.sh
+$SRC_DIR/package_generic_build.sh
