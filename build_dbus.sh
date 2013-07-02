414f184e18d42111e2fbb8733da0d824fb3ec2b0
Skrilax_CZ <skrilax@gmail.com>
2013-07-02 17:38:05 +0200

    Add dbus

---
+++ build_dbus.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## VARIABLES
+
+export SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export ROOT_DIR=$SRC_DIR/../build
+
+export PACKAGE=dbus-1.6.12
+export PACKAGE_ARGS="--sysconfdir=/etc --localstatedir=/var --libexecdir=/usr/lib/dbus-1.0 --with-console-auth-dir=/run/console --enable-abstract-sockets --disable-static"
+export LOGFILE=dbus.log
+
+## Package
+source $SRC_DIR/package_check.sh
+$SRC_DIR/package_generic_build.sh
