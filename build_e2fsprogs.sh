c0e4cb6d7c8fb75494f5dfabaface534d66354b8
Skrilax_CZ <skrilax@gmail.com>
2013-07-02 23:29:37 +0200

    Simplify build for e2fsprogs

--- build_e2fsprogs.sh
+++ build_e2fsprogs.sh
@@ -7,117 +7,19 @@ export ROOT_DIR=$SRC_DIR/../build
 
 export PACKAGE=e2fsprogs-1.42.8
 export PACKAGE_ARGS="--with-root-prefix=\"\" --enable-elf-shlibs"
+export PACKAGE_INSTALL_USE_DESTDIR="1"
 export LOGFILE=e2fsprogs.log
 
 ## Package
 source $SRC_DIR/package_check.sh
+source $SRC_DIR/package_build_func.sh
 
-## TEST
+verify_variables
 
-if [ "$PACKAGE" == "" ]; then
-	echo "PACKAGE is not defined"
-	exit 1
-fi
+configure_package
 
-if [ "$SRC_DIR" == "" ]; then
-	echo "SRC_DIR is not defined"
-	exit 1
-fi
+make_package
 
-if [ "$ROOT_DIR" == "" ]; then
-	echo "ROOT_DIR is not defined"
-	exit 1
-fi
+install_package
 
-if [ "$TRIPLET" == "" ]; then
-	echo "TRIPLET is not defined"
-	exit 1
-fi
-
-if [ "$LOG" == "" ]; then
-	echo "LOG is not defined"
-	exit 1
-fi
-
-if [ ! -d $ROOT_DIR/$TRIPLET/obj ]; then
-	echo "OBJ $ROOT_DIR/$TRIPLET/obj does not exist! Build toolchain first!"
-fi
-
-## CONFIGURE
-
-cd $ROOT_DIR/$TRIPLET/obj
-rm -rf $PACKAGE
-
-if [ "$PACKAGE_COPY_SOURCE" == "1" ]; then
-	cp -a $SRC_DIR/$PACKAGE $ROOT_DIR/$TRIPLET/obj
-	cd $PACKAGE
-	CFG_ROOT=$ROOT_DIR/$TRIPLET/obj/$PACKAGE
-else
-	mkdir $PACKAGE
-	cd $PACKAGE
-	CFG_ROOT=$SRC_DIR/$PACKAGE
-fi
-
-if [ "$PACKAGE_CONFIG_CACHE" != "" ]; then
-	if [ -f "$PACKAGE_CONFIG_CACHE" ]; then
-		echo "Using config.cache from $PACKAGE_CONFIG_CACHE" >> $LOG
-		cat $PACKAGE_CONFIG_CACHE >> $LOG
-		mv $PACKAGE_CONFIG_CACHE config.cache
-		PACKAGE_ARGS="$PACKAGE_ARGS --cache-file=config.cache"
-	else
-		echo "config.cache defined but not found!" >> $LOG
-		echo "config.cache defined but not found!"
-		exit 1
-	fi
-fi
-
-if [ "$PACKAGE_PREFIX" == "" ]; then
-	PACKAGE_PREFIX=/usr
-fi
-
-if [ "$PACKAGE_OMIT_CONFIG_HOST" == "1" ]; then
-	COMPILER_ARGS="CC=$TRIPLET-gcc CXX=$TRIPLET-g++"
-	HOST_ARGS=
-else
-	COMPILER_ARGS=
-	HOST_ARGS="--host=$TRIPLET"
-fi
-
-echo "Configuring $PACKAGE" >> $LOG
-echo "$COMPILER_ARGS $CFG_ROOT/configure --prefix=$PACKAGE_PREFIX $HOST_ARGS $PACKAGE_ARGS" >> $LOG
-eval $COMPILER_ARGS $CFG_ROOT/configure --prefix=$PACKAGE_PREFIX $HOST_ARGS $PACKAGE_ARGS
-
-if [ $? -ne 0 ]; then
-	echo "$PACKAGE configuring failed!" >> $LOG
-	echo "$PACKAGE configuring failed!"
-	exit 1
-fi
-
-## MAKE
-echo "make" >> $LOG
-make
-
-if [ $? -ne 0 ]; then
-	echo "$PACKAGE compiling failed!" >> $LOG
-	echo "$PACKAGE compiling failed!"
-	exit 1
-fi
-
-## INSTALL
-echo "make DESTDIR=$ROOT_DIR/$TRIPLET/sysroot install" >> $LOG
-eval make DESTDIR=$ROOT_DIR/$TRIPLET/sysroot install
-
-if [ $? -ne 0 ]; then
-	echo "$PACKAGE installing failed!" >> $LOG
-	echo "$PACKAGE installing failed!"
-	exit 1
-fi
-
-echo "make DESTDIR=$ROOT_DIR/$TRIPLET/sysroot install-libs" >> $LOG
-eval make DESTDIR=$ROOT_DIR/$TRIPLET/sysroot install-libs
-
-if [ $? -ne 0 ]; then
-	echo "$PACKAGE installing failed!" >> $LOG
-	echo "$PACKAGE installing failed!"
-	exit 1
-fic
+install_package_custom_cmd "install-libs"
