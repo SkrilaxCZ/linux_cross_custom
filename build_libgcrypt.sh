4218285b1e0a5cd30d1844e39b137a0bfc36ddc0
Skrilax_CZ <skrilax@gmail.com>
2013-07-02 18:45:33 +0200

    Add libgcrypt

---
+++ build_libgcrypt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## VARIABLES
+
+export SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export ROOT_DIR=$SRC_DIR/../build
+
+export PACKAGE=libgcrypt-1.5.2
+export PACKAGE_ARGS="--disable-static"
+export LOGFILE=libgcrypt.log
+
+## Package
+source $SRC_DIR/package_check.sh
+$SRC_DIR/package_generic_build.sh
