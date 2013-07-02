879ec01610db0c49d24ad51a983706ac4b4f9b98
Skrilax_CZ <skrilax@gmail.com>
2013-07-02 22:49:06 +0200

    Fix derp in pkgconfig

--- build_pkgconfig.sh
+++ build_pkgconfig.sh
@@ -6,9 +6,9 @@ export SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export ROOT_DIR=$SRC_DIR/../build
 
 export PACKAGE=pkg-config-0.28
-export PACKAGE_ARGS="--docdir=/usr/share/doc/pkg-config-0.28 --disable-host-tool"
 export LOGFILE=pkgconfig.log
 
 ## Package
 source $SRC_DIR/package_check.sh
+export PACKAGE_ARGS="--docdir=$ROOT_DIR/$TRIPLET/sysroot/usr/share/doc/pkg-config-0.28 --disable-host-tool"
 $SRC_DIR/package_generic_build.sh
