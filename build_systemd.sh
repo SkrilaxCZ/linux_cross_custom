797903e855fd85b6c8331989b5ec9c8b9374def4
Skrilax_CZ <skrilax@gmail.com>
2013-07-03 08:26:10 +0200

    Fix build for systemd

--- build_systemd.sh
+++ build_systemd.sh
@@ -7,8 +7,16 @@ export ROOT_DIR=$SRC_DIR/../build
 
 export PACKAGE=systemd-204
 export LOGFILE=systemd.log
+export PACKAGE_CONFIG_CACHE="/tmp/systemd.cache"
+export PACKAGE_INSTALL_USE_DESTDIR="1"
+
+# Config cache
+cat > $PACKAGE_CONFIG_CACHE << "EOF"
+ac_cv_func_malloc_0_nonnull=yes
+ac_cv_func_realloc_0_nonnull=yes
+EOF
 
 ## Package
 source $SRC_DIR/package_check.sh
-export PACKAGE_ARGS="--with-libgcrypt-prefix=$ROOT_DIR/$TRIPLET/sysroot/usr --disable-nls --with-distro=other --libexecdir=/usr/lib --localstatedir=/var --sysconfdir=/etc"
+export PACKAGE_ARGS="--with-libgcrypt-prefix=$ROOT_DIR/$TRIPLET/sysroot/usr --disable-nls --libexecdir=/usr/lib --localstatedir=/var --sysconfdir=/etc --without-python"
 $SRC_DIR/package_generic_build.sh
