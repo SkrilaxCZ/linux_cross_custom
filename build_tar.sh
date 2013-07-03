42b0a8dd089ab41f1413ad95acbd17609cae67e4
Skrilax_CZ <skrilax@gmail.com>
2013-07-03 21:55:38 +0200

    Add tar

---
+++ build_tar.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## VARIABLES
+
+export SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export ROOT_DIR=$SRC_DIR/../build
+
+export PACKAGE=tar-1.26
+export PACKAGE_CONFIG_CACHE="/tmp/tar.cache"
+export LOGFILE=tar.log
+
+# Config cache
+cat > $PACKAGE_CONFIG_CACHE << "EOF"
+gl_cv_func_wcwidth_works=yes
+gl_cv_func_btowc_eof=yes
+ac_cv_func_malloc_0_nonnull=yes
+ac_cv_func_realloc_0_nonnull=yes
+gl_cv_func_mbrtowc_incomplete_state=yes
+gl_cv_func_mbrtowc_nul_retval=yes
+gl_cv_func_mbrtowc_null_arg=yes
+gl_cv_func_mbrtowc_retval=yes
+gl_cv_func_wcrtomb_retval=yes
+EOF
+
+## Package
+source $SRC_DIR/package_check.sh
+$SRC_DIR/package_generic_build.sh
